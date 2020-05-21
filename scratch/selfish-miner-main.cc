38ee25f82dc741449cd5eb125d84fec62ba70f6d
hiddenfigure96 <hiddenfigure96@gmail.com>
2020-05-21 12:26:50 +0430

    new selfish mining init commit

---
+++ scratch/selfish-miner-main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/point-to-point-layout-module.h"
+
+int blockIntervalMinutes;
+int blockNumber;
+int iterations;
+
+
+int main()
+{
+    std::cout << "initial commit" << std::endl;
+    return 0;
+}
