348ee1e0521850a1d5d109752e604f341ec2b900
hiddenfigure96 <hiddenfigure96@gmail.com>
2020-05-21 11:11:32 +0430

    Changes for running simulator

--- copy-to-ns3.sh
+++ copy-to-ns3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Configure the following values
-RAPIDJSON_FOLDER=~/rapidjson
-NS3_FOLDER=~/workspace/ns-allinone-3.25/ns-3.25
+RAPIDJSON_FOLDER=~/blockchain/NS3/rapidjson
+NS3_FOLDER=~/blockchain/NS3/ns-allinone-3.25/ns-3.25
 
-# Do not change
-mkdir $NS3_FOLDER/rapidjson
+# Do not change, uncomment if you run this for the first time
+# mkdir $NS3_FOLDER/rapidjson
+
 cp  -r $RAPIDJSON_FOLDER/include/rapidjson/* $NS3_FOLDER/rapidjson/
 cp  src/applications/model/* $NS3_FOLDER/src/applications/model/
 cp  src/applications/helper/* $NS3_FOLDER/src/applications/helper/
